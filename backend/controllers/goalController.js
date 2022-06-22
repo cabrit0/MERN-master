3eb9c05b09e1206793146553c1712b078a74104d
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-22 19:31:19 +0100

    first

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,49 @@
+const asyncHandler = require("express-async-handler");
+
+// @desc   Get Goals
+// @route  GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "get goals",
+  });
+});
+
+// @desc   Set Goals
+// @route  Post /api/goals
+// @access Private
+const createGoal = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  if (!req.body) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  res.status(200).json({
+    message: "goal created",
+  });
+});
+
+// @desc   Update Goals
+// @route  Put /api/goals/:id
+// @access Private
+const updateGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `update goal ${req.params.id}`,
+  });
+});
+
+// @desc   Delete Goals
+// @route  DELETE /api/goals/:id
+// @access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `delete goal ${req.params.id}`,
+  });
+});
+
+module.exports = {
+  getGoals,
+  createGoal,
+  updateGoal,
+  deleteGoal,
+};
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//choose type of parser
+app.use(bodyParser.text({ type: "*/*" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
