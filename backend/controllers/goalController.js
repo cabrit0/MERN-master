284bf85f17885f2c789c6193dab5a995d6ea52b8
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-24 19:09:11 +0100

    initial rest goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // @desc   Get Goals
 // @route  GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "get goals",
-  });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 // @desc   Set Goals
@@ -18,27 +19,47 @@ const createGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({
-    message: "goal created",
-  });
+
+  const goal = await Goal.create({ text: req.body.text });
+  res.status(200).json(goal);
 });
 
 // @desc   Update Goals
 // @route  Put /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `update goal ${req.params.id}`,
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
   });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc   Delete Goals
 // @route  DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `delete goal ${req.params.id}`,
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  res.json({
+    message: "Goal deleted",
   });
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 //choose type of parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 
